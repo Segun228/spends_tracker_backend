193d95622113893c2b5e93c7171ee302741550b9
Segun228 <segunperkele@gmail.com>
2025-08-11 17:16:18 +0300

    applying migrations

--- backend/users/models.py
+++ backend/users/models.py
@@ -8,7 +8,12 @@ class User(AbstractUser):
     updated_at = models.DateTimeField(auto_now=True)
     is_admin = models.BooleanField(default=False)
     is_alive = models.BooleanField(default=True)
-
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        blank=True,
+        null=True,
+    )
     def __str__(self):
         return self.username
 
