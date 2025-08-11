b26a607ea0a0e30b72da433bc4da32a7e1cfa876
Segun228 <segunperkele@gmail.com>
2025-08-11 15:57:06 +0300

    adding extra route

--- backend/users/views.py
+++ backend/users/views.py
@@ -4,6 +4,8 @@ from rest_framework.generics import (
     ListCreateAPIView, RetrieveUpdateDestroyAPIView
 )
 from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class UserListCreateView(ListCreateAPIView):
@@ -16,3 +18,19 @@ class UserRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = [IsAdminUser]
+    def get_object(self):
+        if 'telegram_id' in self.kwargs:
+            return self.queryset.get(telegram_id=self.kwargs['telegram_id'])
+        
+        return super().get_object()
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except User.DoesNotExist:
+            return Response(
+                {"detail": "User with this telegram_id does not exist."},
+                status=status.HTTP_404_NOT_FOUND
+            )
